c45ad6c83fb9810e565fc873220712967dce9bc7
AaryanChandrakar <146125867+AaryanChandrakar@users.noreply.github.com>
2024-03-12 08:31:25 +0000

    datatypes

---
+++ Day2/script.js
@@ -0,0 +1,25 @@
+                                 // Data type in JS
+
+"use strict"; // Treat all JS code as newer version (ye nhi lekhenge to bhi machine ko pta chal jata h by default)
+              // Par likhna accha hota h.
+//alert (3 + 3)    // we are using node.js not browser
+
+let name = "Bhupendra"           // string => ""
+let age = 54                     // nuber => 2 to the power 53
+let loggedIn = false             //  boolean  => true or false
+let state;                       // undefined => variable declear hua par valu asign ni hua to undefined hoga
+let interest = null              // null => stand alone value
+
+                // ye sab premitive data type h
+// nuber => 2 to the power 53
+// bigint 
+// string => ""
+//  boolean  => true or false
+// null => stand alone value
+// undefined => variable declear hua par valu asign ni hua to undefined hoga
+// symbol => unique
+
+//console.table([name, age, loggedIn, state, interest]);
+
+console.log(typeof interest); // object
+console.log(typeof state); // undefined
