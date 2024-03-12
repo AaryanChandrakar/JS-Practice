1b38c79c32bc629c16b99cf1bff0ec55b83f7b76
AaryanChandrakar <146125867+AaryanChandrakar@users.noreply.github.com>
2024-03-12 15:17:01 +0000

    Conversion of datatype

--- Day2/script.js
+++ Day2/script.js
@@ -21,5 +21,70 @@ let interest = null              // null => stand alone value
 
 //console.table([name, age, loggedIn, state, interest]);
 
-console.log(typeof interest); // object
-console.log(typeof state); // undefined
+//console.log(typeof interest); // object
+//console.log(typeof state); // undefined
+
+                   
+
+                                        // Datatype Conversion
+
+let score = 50
+console.log(typeof(score));
+
+let valueInNum = Number(score)
+console.log(typeof(valueInNum));
+console.log(valueInNum);
+
+console.log("--------------------------")
+
+
+let score1 = "Ram123"
+console.log(typeof(score1))
+
+let valueInNum1 = Number(score1)
+console.log(typeof(valueInNum1));
+console.log(valueInNum1);
+
+
+console.log("--------------------------")
+
+
+let score2 = null
+console.log(typeof(score2));
+
+let valueInNum2 = Number(score2)
+console.log(typeof(valueInNum2));
+console.log(valueInNum2);
+
+
+console.log("--------------------------")
+
+
+let score3 = 0
+console.log(typeof(score2));
+
+let valueInNum3 = Boolean(score3)
+console.log(typeof(valueInNum3));
+console.log(valueInNum3);
+
+
+console.log("--------------------------")
+
+
+let score4 = "Shyam"
+console.log(typeof(score4));
+
+let valueInNum4 = Boolean(score4)
+console.log(typeof(valueInNum4));
+console.log(valueInNum4);
+
+
+console.log("--------------------------")
+
+
+let score5 = 100
+console.log(typeof(score5));
+
+let valueInNum5 = String(score5)
+console.log(typeof(valueInNum5));
+console.log(valueInNum5);
